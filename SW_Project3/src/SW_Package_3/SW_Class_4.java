a848c7944895000b857018f49913374edd03e676
vinoa <vinoa@LAPTOP-HLJHU4OD.hub>
2024-09-19 22:27:29 +1200

    Project 3 commit 3

--- SW_Project3/src/SW_Package_3/SW_Class_4.java
+++ SW_Project3/src/SW_Package_3/SW_Class_4.java
@@ -2,7 +2,7 @@ package SW_Package_3;
 
 public class SW_Class_4 
 {public static void main(String[] args) 
-{SW_Class_4 Ob = new SW_Class_4 ();
-System.out.println("Hello World!");
+{System.out.println("Hello World!");
+System.out.println("Hello New Zealand!");
 }
 }
